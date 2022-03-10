de2b7d00831262994beee165896f018eb0cd295d
SaraswathiC <saramba06@gmail.com>
2022-03-10 13:32:28 +0530

    12345

---
+++ src/org/git/Git2pm.java
@@ -0,0 +1,13 @@
+package org.git;
+
+public class Git2pm {
+private void add() {
+System.out.println("123");
+}
+public static void main(String[] args) {
+	Git2pm g = new Git2pm();
+	
+			g.add();
+	
+}
+}
